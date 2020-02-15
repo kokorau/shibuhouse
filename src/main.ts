a3ceee191e4b33829a3e68a2613568f04860ce21
kokorau <kokorau@gmail.com>
2020-02-16 06:16:58 +0900

    fix

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,5 @@
 import Vue from 'vue'
 import VueLazyLoad from 'vue-lazyload'
-import './plugins/axios'
 import App from './App.vue'
 import router from './router'
 import store from './store'
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,58 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <AppMenu></AppMenu>
+    <ImageLayout class="images" :images="images"></ImageLayout>
   </div>
 </template>
 
-<script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+<script lang="ts">
+import * as Contentful from 'contentful'
+import { Component, Vue } from 'vue-property-decorator'
+import ImageLayout from '@/components/ImageLayout.vue'
+import AppMenu from '@/components/AppMenu.vue'
 
-export default {
-  name: 'Home',
+const client = Contentful.createClient({
+  space: '3phzeo0bdyyi',
+  accessToken: '7e7552e8f2ba5ffef4f7330770c26536069c5e86c7066dd3540ab71244cf1884'
+})
+
+@Component({
   components: {
-    HelloWorld
+    AppMenu,
+    ImageLayout
+  }
+})
+export default class Home extends Vue {
+  images: any = [
+    {
+      src: 'https://thispersondoesnotexist.com/image'
+    }
+  ]
+  async mounted() {
+    const imageRef = await client.getEntries({
+      content_type: 'home',
+      order: '-sys.createdAt'
+    })
+    this.images = imageRef.items.map(i => ({
+      // @ts-ignore
+      src: i.fields.image.fields.file.url
+    }))
   }
 }
 </script>
+
+<style scoped lang="sass">
+@media screen and (max-width: 480px)
+  .home::after
+    content: 'SHIBUHOUSE'
+    position: fixed
+    top: 50%
+    left: 50%
+    transform: translate(-50%, -50%)
+    z-index: 100
+    text-align: center
+    font-size: 10vh
+    color: transparent
+    background: linear-gradient(3deg, #151515, #151515 47%, transparent 48%, transparent 52%, #151515 53%, #151515)
+    -webkit-background-clip: text
+</style>
